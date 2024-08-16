e6fc03ff9844bd7a8c8e7c895363611b197cf8c5
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2024-08-17 02:06:20 +0700

    Fix upstream title

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -9,7 +9,7 @@ export async function GET(request: NextRequest) {
 
     if (!code && !refreshToken)
         redirect(
-            "https://discord.com/oauth2/authorize?client_id=1115953141146464276&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth&scope=identify+guilds+guilds.members.read"
+            "https://discord.com/oauth2/authorize?client_id=1115953141146464276&response_type=code&redirect_uri=https%3A%2F%2Flive.tryz.id.vn%2Fapi%2Fauth&scope=identify+guilds+guilds.members.read"
         );
 
     const oauth = new DiscordOauth2();
@@ -19,7 +19,7 @@ export async function GET(request: NextRequest) {
             ? await oauth.tokenRequest({
                   clientId: process.env.CLIENT_ID,
                   clientSecret: process.env.CLIENT_SECRET,
-                  redirectUri: "http://localhost:3000/api/auth",
+                  redirectUri: "https://live.tryz.id.vn/api/auth",
                   refreshToken,
                   grantType: "refresh_token",
                   scope: "identify guilds guilds.members.read",
@@ -27,7 +27,7 @@ export async function GET(request: NextRequest) {
             : await oauth.tokenRequest({
                   clientId: process.env.CLIENT_ID,
                   clientSecret: process.env.CLIENT_SECRET,
-                  redirectUri: "http://localhost:3000/api/auth",
+                  redirectUri: "https://live.tryz.id.vn/api/auth",
                   code,
                   scope: "identify guilds guilds.members.read",
                   grantType: "authorization_code",
